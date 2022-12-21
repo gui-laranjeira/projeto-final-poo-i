f3bd2c56df07e16df4b86c3765e5c2715cc4b533
Guilherme Laranjeira <guilr14@gmail.com>
2022-12-21 19:15:00 -0300

    Adicionar Conta Abstrata

---
+++ conta-bancaria/Models/Conta.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conta_bancaria.Models
+{
+    internal abstract class Conta
+    {
+        public Conta(int numeroConta, Cliente cliente)
+        {
+            NumeroConta = numeroConta;
+            Cliente = cliente;
+            Saldo = 0;
+        }
+
+        public int NumeroConta { get; set; }
+        public double Saldo { get; set; }
+        public Cliente Cliente { get; set; }
+        public List<double> Movimentacoes { get; set; }
+
+        public double Depositar(double valor)
+        {
+            Movimentacoes.Add(valor);
+            Saldo += valor;
+            return Saldo;
+        }
+
+        public double Sacar(double valor)
+        {
+            valor *= (-1);
+            Movimentacoes.Add(valor);
+            Saldo += valor;
+            return Saldo;
+        }
+
+        public void Extrato()
+        {
+            Console.WriteLine("Extrato: \n");
+            foreach(var item in Movimentacoes)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Saldo total: {Saldo}");
+        }
+
+        public virtual double CalcularValorTarifaManutencao()
+        {
+            double taxa = 1;
+            double tarifa = taxa * (Saldo / 100);
+            return tarifa;
+        }
+    }
+}
--- conta-bancaria/Models/Holerite.cs
+++ conta-bancaria/Models/Holerite.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace conta_bancaria
+namespace conta_bancaria.Models
 {
-    internal class Conta
+    internal class Holerite
     {
     }
 }
