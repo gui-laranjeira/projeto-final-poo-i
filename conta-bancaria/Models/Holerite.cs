cb48ff0e8cdaaf8259f2e5a54ad1de1c2699ccd4
KaioMazzotti <kaio.mazzotti@gmail.com>
2022-12-22 11:57:27 -0300

    holerite pt 1

--- conta-bancaria/Models/Holerite.cs
+++ conta-bancaria/Models/Holerite.cs
@@ -8,5 +8,33 @@ namespace conta_bancaria.Models
 {
     internal class Holerite
     {
+        public Holerite(string nomeEmpresa, int cnpjEmpresa, string endereçoEmpresa, string cargoEmpresa)
+        {
+            Console.WriteLine("Nos informe o Holerite para criação da sua conta salário! ");
+            Console.WriteLine("Digite o CNPJ da Empresa: ");
+            cnpjEmpresa = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a Razão Social: ");
+            nomeEmpresa = Console.ReadLine();
+            Console.WriteLine("Digite o Endereço da Empresa: ");
+            endereçoEmpresa = Console.ReadLine();
+            Console.WriteLine("Digite o seu Cargo/Função na empresa: ");
+            cargoEmpresa = Console.ReadLine();
+        }
+        //Formatação do holerite:
+        //string holeriteCompleto = @$"
+        //EMPREGADOR                                                    RECIBO DE PAGAMENTO DE SALÁRIO
+        //{nomeEmpresa}                                              
+        //ENDEREÇO: {endereçoEmpresa}
+        //CNPJ: {cnpjEmpresa}
+        //
+        //FUNCIONÁRIO: {nomeFuncionario}  FUNÇÃO: {cargoEmpresa}
+        //
+        //
+        //SALÁRIO BASE                                                                             R$
+        //IMPOSTOS                                                                                 R$
+        //
+        //
+        //                                                              SALÁRIO LÍQUIDO:           R$
+        //";
     }
 }
