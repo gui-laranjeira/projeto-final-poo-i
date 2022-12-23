766167350613654bcc84d6ccc2ae83e8061c4285
Jessica Pansini <jessica.pansini@rsmbrasil.com.br>
2022-12-23 20:10:44 -0300

    Avaliar Perfil Investidor

--- conta-bancaria/Models/ContaInvestimento.cs
+++ conta-bancaria/Models/ContaInvestimento.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,51 @@ namespace conta_bancaria.Models
 {
     public class ContaInvestimento : Conta
     {
-        public double taxaManutencao = 0.08;
+        public double taxaDeManutencao = 0.08;
+  
+
 
         public ContaInvestimento(int numeroConta, Cliente cliente) : base(numeroConta, cliente)
         {
         }
 
-       
+
+        public string avaliarPerfilInvestidor(double valorDisponivel)
+        {
+            bool check;
+
+            Console.WriteLine("\nDefina seu perfil de investidor! ");
+            Console.WriteLine("Qual valor tem disponível para investimento?");
+            check = double.TryParse(Console.ReadLine(), out valorDisponivel);
+
+            if (valorDisponivel > 500)
+            {
+                return "Conservador";
+            }
+            else if(valorDisponivel > 1000)
+            {
+                return "Moderado";
+            }
+            else
+            {
+                return "Agressivo";
+            }
+        }
+
+        public double InvestirEmAcoes(double investimento)
+        {
+
+
+            return investimento;
+        }
+
+        public override double CalcularValorTarifaManutencao()
+        {
+            double tarifa = this.taxaDeManutencao * (Saldo / 100);
+            return tarifa;
+        }
+
+
+
     }
 }
